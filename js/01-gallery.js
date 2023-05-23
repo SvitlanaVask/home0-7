8845a280f018bccc37149947eaea314e45a92de0
SvitlanaVas <svetaklymchuk@gmail.com>
2023-05-23 18:28:39 +0300

    add 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,2 +1,42 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryBoxRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryBoxRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+galleryBoxRef.addEventListener('click', onGalleryImageClick)
+
+function createGalleryMarkup(items) { 
+  return items.map(({original, preview, description }) => {
+    return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+   `
+  }).join('');
+};
+
+function onGalleryImageClick(evt) {
+  evt.preventDefault();
+  
+  const originalImgUrl = evt.target.dataset.source;  
+  
+  basicLightbox.create(`
+		<img width="860" src="${originalImgUrl}">
+	`).show()
+
+  if (!evt.target.classList.contains('gallery__image')) {
+      return
+    }
+  
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,24 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryBoxRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryBoxRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+  captionsData: "alt",
+  captionDelay: 200,
+});
+
+function createGalleryMarkup(items) { 
+  return items.map(({original, preview, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+   `
+  }).join('');
+};
 
